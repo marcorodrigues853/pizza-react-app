3042f87fca39a048c5846d98289b853c79f1aa66
Marco Rodrigues <marcoaurelio853@gmail.com>
2023-07-09 22:07:30 +0100

    core: add geolocation and errors

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -56,7 +56,8 @@ const userSlice = createSlice({
       })
       .addCase(fetchAddress.rejected, (state, action) => {
         state.status = 'error';
-        state.error = action.error.message;
+        state.error =
+          'There was a problem getting your address, please fill it out and try again!';
       }),
 });
 
